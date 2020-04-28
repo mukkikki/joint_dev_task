5cd1d0b43ef3b3bcc7ffb461eab1c08d2cb40e91
向井正 <mukaitadashi@mukaitadashinoMacBook-Pro.local>
2020-04-28 23:21:36 +0900

    Q15 re:answer

--- task.rb
+++ task.rb
@@ -133,17 +133,8 @@ def q15
   data1 = { name: "saitou", hobby: "soccer", age: 33, role: "admin" }
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
-  if data1.has_key?(:age)
-    puts "OK"
-  else
-    puts "NG"
-  end
-  
-  if data2.has_key?(:age)
-    puts "OK"  
-  else
-    puts "NG"
-  end
+  puts data1.has_key?(:age)? "OK" : "NG"
+  puts data2.has_key?(:age)? "OK" : "NG"
 
 end
 
