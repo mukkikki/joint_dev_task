67d5a0abea506dc8a6010e5de7cc5de72590c006
向井正 <mukaitadashi@mukaitadashinoMacBook-Pro.local>
2020-05-08 23:32:25 +0900

    Q17~Q20 answer

--- task.rb
+++ task.rb
@@ -182,7 +182,14 @@ def q17
 end
 
 class UserQ18
-  # 以下に回答を記載
+  def initialize(**params)
+    @name = params[:name]
+    @age = params[:age] 
+  end
+
+  def introduce
+    @age > 10? "こんにちは，#{@name}と申します。宜しくお願いいたします。" : "はいさいまいど〜，#{@name}です！！！"
+  end
 
 end
 
@@ -197,9 +204,10 @@ end
 
 class Item
   # 以下を修正して下さい
+  attr_reader :name
 
   def initialize(name)
-    @name = name
+    @name = name[:name]
   end
 end
 
@@ -210,12 +218,39 @@ def q19
 end
 
 class UserQ20
-  # 以下に回答を記載
-
+  attr_reader :name ,:age
+ def initialize(name:,age:)
+    @name =name
+    @age =age
+ end
+  
 end
 
 class Zoo
-  # 以下に回答を記載
+  attr_reader :name, :entry_fee
+
+  def initialize(name:,entry_fee:)
+      @name = name
+      @entry_fee = entry_fee
+  end
+  def info_entry_fee(user)
+
+      fee = case user.age
+      when 0..5
+        @entry_fee[:infant]
+        
+      when 6..12
+        @entry_fee[:children]
+      when 13..64
+        @entry_fee[:adult]
+      else
+        @entry_fee[:senior]
+        
+      end
+      puts "#{user.name}さんの入場料金は#{fee}円です"
+  end
+
+    
 
 end
 
